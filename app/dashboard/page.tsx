2de3d277a9a5c936e5a88e1430fa7732d9a71e94
fradelycodes <fradelydventura@gmail.com>
2023-11-16 04:03:38 -0400

    add dynamic rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,10 +12,10 @@ export default async function Page() {
 	const revenue = await fetchRevenue();
 	const latestInvoices = await fetchLatestInvoices();
 	const {
-		totalPaidInvoices,
-		totalPendingInvoices,
 		numberOfInvoices,
 		numberOfCustomers,
+		totalPaidInvoices,
+		totalPendingInvoices,
 	} = await fetchCardData();
 	return (
 		<main>
