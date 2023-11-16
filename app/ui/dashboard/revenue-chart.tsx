aac8077af17382b6aa908291068c6b7303c50a6b
fradelycodes <fradelydventura@gmail.com>
2023-11-16 03:17:25 -0400

    add revenue and latest invoices graphics and data

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -17,7 +17,7 @@ export default async function RevenueChart({
 	const chartHeight = 350;
 	// NOTE: comment in this code when you get to this point in the course
 
-	// const { yAxisLabels, topLabel } = generateYAxis(revenue);
+	const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
 	// if (!revenue || revenue.length === 0) {
 	//   return <p className="mt-4 text-gray-400">No data available.</p>;
@@ -32,23 +32,23 @@ export default async function RevenueChart({
 
 			<div className="rounded-xl bg-gray-50 p-4">
 				<div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-					{/* <div
+					<div
 						className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
 						style={{ height: `${chartHeight}px` }}
 					>
 						{yAxisLabels.map((label) => (
 							<p key={label}>{label}</p>
 						))}
-					</div> */}
+					</div>
 
 					{revenue.map((month) => (
 						<div key={month.month} className="flex flex-col items-center gap-2">
-							{/* <div
+							<div
 								className="w-full rounded-md bg-blue-300"
 								style={{
 									height: `${(chartHeight / topLabel) * month.revenue}px`,
 								}}
-							></div> */}
+							></div>
 							<p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
 								{month.month}
 							</p>
